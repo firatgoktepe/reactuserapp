3274d1b595bc98bbca9f44908c73252934e9367d
firatgoktepe <47864126+firatgoktepe@users.noreply.github.com>
2019-04-04 12:33:31 +0300

    UserApp built by ReactJS

---
+++ src/pages/Contribute.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contribute() {
+  return (
+    <div>
+      <h3>Project Files and Contribute to Project</h3>
+      <p>You can download full project from <a href ="https://github.com/firatgoktepe/reactapp">this</a> github page. Also, you can contribute to this project.</p>
+    </div>
+  )
+}
+export default Contribute;
